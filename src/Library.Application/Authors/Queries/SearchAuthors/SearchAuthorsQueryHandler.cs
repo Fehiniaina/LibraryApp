d50d403598a727db6163748d7b0f6effd9e97a2a
Fehiniaina RAHARINIVOSON <fehiniaina.raharinivoson@versusmind.eu>
2026-09-07 17:29:19 +0400

    Pagination

--- src/Library.Application/Authors/Queries/SearchAuthors/SearchAuthorsQueryHandler.cs
+++ src/Library.Application/Authors/Queries/SearchAuthors/SearchAuthorsQueryHandler.cs
@@ -13,6 +13,10 @@ public class SearchAuthorsQueryHandler : IRequestHandler<SearchAuthorsQuery, Pag
 
     public async Task<PagedResult<AuthorDto>> Handle(SearchAuthorsQuery request, CancellationToken ct)
     {
+        const int MaxPageSize = 100;
+        var pageSize = Math.Clamp(request.PageSize, 1, MaxPageSize);
+        var page = Math.Max(request.Page, 1);
+
         IQueryable<Author> query = _db.Authors.AsNoTracking();
 
         if (!string.IsNullOrEmpty(request.LastName))
@@ -29,11 +33,11 @@ public class SearchAuthorsQueryHandler : IRequestHandler<SearchAuthorsQuery, Pag
 
         var items = await query
             .OrderBy(a => a.Id)
-            .Skip((request.Page - 1) * request.PageSize)
-            .Take(request.PageSize)
+            .Skip((page - 1) * pageSize)
+            .Take(pageSize)
             .Select(a => new AuthorDto(a.Id, a.FirstName, a.LastName, a.Books.Count))
             .ToListAsync(ct);
 
-        return new PagedResult<AuthorDto>(items, totalCount, request.Page, request.PageSize);
+        return new PagedResult<AuthorDto>(items, totalCount, page, pageSize);
     }
 }
